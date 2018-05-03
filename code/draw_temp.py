5726992486dcf204625fbc058e37405dfe8b61d9
jshn232 <jshn232@gmail.com>
2018-05-03 22:49:18 +0800

    支持刻度&x轴间隔

--- code/draw_temp.py
+++ code/draw_temp.py
@@ -1,11 +1,11 @@
 # -*- coding: UTF-8 -*-
 import numpy as np
 import pylab as pl
+from matplotlib.ticker import MultipleLocator
+from matplotlib.ticker import FormatStrFormatter
 import time
 import os
-x1=[]
-x2=[]
-x3=[]
+x=[]
 y1=[]
 y2=[]
 y3=[]
@@ -16,21 +16,23 @@ with open(filename,'r') as f:
 		strlist = s.split(',')
 		if len(strlist) == 4:
 			rTime = time.strptime(strlist[0],'%Y-%m-%d %H:%M:%S')
-			strTime = str.format("%02d:%02d"%(rTime.tm_hour,rTime.tm_min))
-			x1.append(strTime)
-			x2.append(strTime)
-			x3.append(strTime)
+			strTime = str.format("%04d/%02d/%02d %02d:%02d" % \
+			                     (rTime.tm_year,rTime.tm_mon,rTime.tm_mday,rTime.tm_hour, rTime.tm_min))
+			x.append(strTime)
 			y1.append(float(strlist[1]))
 			y2.append(float(strlist[2]))
 			y3.append(float(strlist[3]))
 
-
-pl1, = pl.plot(x1, y1,'r')# use pylab to plot x and y
-
-pl2, = pl.plot(x2, y2,'b')# use pylab to plot x and y
-
-pl3, = pl.plot(x3, y3,'y')
+pl1, = pl.plot(x, y1,'r')# use pylab to plot x and y
+pl2, = pl.plot(x, y2,'b')# use pylab to plot x and y
+pl3, = pl.plot(x, y3,'y')
 
 pl.legend([pl1, pl2, pl3], ('cpu_t', 'env_h','env_t'))
+
+xmajorLocator = MultipleLocator(20)  #设置x轴间隔为20
+pl.gca().xaxis.set_major_locator(xmajorLocator)
+pl.gca().xaxis.grid(True, which='major')  # x坐标轴的网格使用主刻度
+
+pl.gcf().autofmt_xdate()  #自适应宽度
 
 pl.show()# show the plot on the screen
