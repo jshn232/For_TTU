bd02921e49ec5f5d858e2e20fe5b53a0773516b8
jshn232 <jshn232@gmail.com>
2021-01-21 10:21:17 +0800

    Delete code/D_FileMaker directory

--- code/D_FileMaker/udisk-down_zip.sh
+++
@@ -1,6 +0,0 @@
-rm /app/download.tar.gz
-rm -fr /app/download
-mv /app/main_1.bin /app/download.tar.gz
-tar -xzvf /app/download.tar.gz -C /app/
-chmod 777 /app/download/setup.sh
-/app/download/setup.sh
